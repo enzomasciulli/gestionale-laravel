4e1b9ede26c62cb90ace2210b1f944d309fe68c9
enzomasciulli <enzo.masciulli@accenture.com>
2018-11-25 19:02:43 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+    <h2 class="text-h2"> Work in progress... </h2>
+    <div class="col-xs-12 text-center">
+        <div class="overlay">
+        <i class="fa fa-refresh fa-spin"></i>
+        </div>
+    </div>
+    
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/app.css">
+@stop
+
+@section('js')
+    <script src="/js/app.js"></script>
+@stop
---
+++ resources/views/sensors.blade.php
@@ -0,0 +1,20 @@
+@extends('adminlte::page')
+
+@section('title', 'Sensori')
+
+@section('content_header')
+    <h1>Sensori</h1>
+@stop
+
+@section('content')
+    <p>Elenco sensori</p>
+    
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    
+@stop
